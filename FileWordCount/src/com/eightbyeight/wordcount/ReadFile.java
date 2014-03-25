ab27171b39bf4698625f066183331a15d3d3b216
nimmicv <nimmicv@gmail.com>
2014-03-24 18:24:54 -0700

    Update ReadFile.java

--- FileWordCount/src/com/eightbyeight/wordcount/ReadFile.java
+++ FileWordCount/src/com/eightbyeight/wordcount/ReadFile.java
@@ -16,6 +16,11 @@ public class ReadFile {
 	
 	public static void main(String[] args)
 	{
+		if(args.length==0)
+		{
+			System.out.println("Arguemtns missing");
+			System.exit(1);
+		}
 		if(args.length > 0) { 
 		 CountWords cws = new CountWords();
 		 // Read all files passed as argument
