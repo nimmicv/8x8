eafa60abe237ac5c31b18ff45b62258ae7c5f4d0
nimmicv <nimmicv@gmail.com>
2014-04-08 19:30:28 -0700

    Adding another way of doing the same

--- Strings/src/com/eightbyeight/String/RemoveDuplicates.java
+++ Strings/src/com/eightbyeight/String/RemoveDuplicates.java
@@ -11,9 +11,13 @@ public class RemoveDuplicates {
 		String duplicateString = "abbccddeghst";
 		RemoveDuplicates rdp = new RemoveDuplicates();
 		String uniqueString = rdp.removeDup(duplicateString);
+		System.out.println("New String =" + uniqueString);
+		String uniqueString2 = rdp.removeDup2(duplicateString);
+		System.out.println("New String =" + uniqueString2);
 
 	}
 	
+	// Using LinkedHashSet
 	public String removeDup(String s)
 	{
 		
@@ -21,7 +25,7 @@ public class RemoveDuplicates {
 		{
 			return null;
 		}
-		
+		// I used a linkedHashset instead of hashset to maintain the order of insertion
 		Set<Character> set = new LinkedHashSet<Character>();
 		for(int i=0;i<s.length();i++)
 		{
@@ -34,5 +38,31 @@ public class RemoveDuplicates {
 		}
 		return sb.toString();
 	}
+	
+	
+	// Using 
+	public String removeDup2(String s)
+	{
+		
+		if(s == null)
+		{
+			return null;
+		}
+		boolean[] array = new boolean[128];
+		StringBuffer sb = new StringBuffer();
+		
+		for(int i=0;i<s.length();i++)
+		{
+			if(!array[(int)s.charAt(i)])
+			{
+				array[(int)s.charAt(i)]=true;
+				sb.append(s.charAt(i));
+			}
+		}
+		//System.out.println(sb.toString());
+		
+		return sb.toString();
+	}
+
 
 }
